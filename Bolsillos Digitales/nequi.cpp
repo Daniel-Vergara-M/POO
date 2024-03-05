aa4d05e2d88737d55e2a00bdafbdd590bbfaab3f
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-03-05 16:20:49 -0500

    Se eliminaron los métodos para evitar adelantarme en lo pedido en la tarea, se creo una matriz para los códigos con valores de dinero, y las demás clases para cada una de las funcionalidades

--- Bolsillos Digitales/nequi.cpp
+++ Bolsillos Digitales/nequi.cpp
@@ -12,31 +12,27 @@ string db[10][4] = {
     {"Katherine", "3106031563", "0505", "200000"},
     {"Juan", "3007464518", "0505", "300000"}};
 
+string bankList[3] = {"bancolombia", "davivienda", "bbva"};
+string validCodes[5][2] = {
+    {"1576", "10000"},
+    {"9374", "20000"},
+    {"7264", "30000"},
+    {"2985", "40000"},
+    {"3875", "50000"}
+};
+string movementsTypes[4] = {"recargar", "transferir", "retirar", "guardar"};
 class Nequi
 {
 private:
     string phoneNumber;
     string password;
     int balance;
+    Vector<Vector<string>> movements;
 
-public:
-    Nequi(){};
-    Nequi(string phoneNumber, string password)
+    bool isValidPassword(string password)
     {
-        if (phoneNumber == "" || password == "")
-        {
-            println("Error: Phone number or password can't be empty");
-            return;
-        }
-        if (phoneNumber.length() != 10 || password.length() != 4)
-        {
-            println("Error: Phone number must have 10 digits and/or password must have 4 digits");
-            return;
-        }
-        this->phoneNumber = phoneNumber;
-        this->password = password;
-        this->balance = this->getBalanceByPhone(phoneNumber);
-    };
+        return this->password == password;
+    }
 
     int getBalanceByPhone(string phoneNumber)
     {
@@ -47,12 +43,125 @@ public:
                 return stoi(db[i][3]);
             }
         }
-        println("Error: Phone number not found");
+        println("\033[31m Error: Phone number not found");
+    }
+
+public:
+    Nequi(){};
+    Nequi(string phoneNumber, string password)
+    {
+        if (phoneNumber == "" || password == "")
+        {
+            println("\033[31m Error: Phone number or password can't be empty");
+            return;
+        }
+        if (phoneNumber.length() != 10 || password.length() != 4)
+        {
+            println("\033[31m Error: Phone number must have 10 digits and/or password must have 4 digits");
+            return;
+        }
+        this->phoneNumber = phoneNumber;
+        this->password = password;
+        this->balance = this->getBalanceByPhone(phoneNumber);
+    };
+
+};
+
+class Recarga
+{
+private:
+    Nequi account;
+    string method;
+    int amount;
+
+public:
+    Recarga() {}
+    Recarga(Nequi account, string method, int amount)
+    {
+        this->account = account;
+        this->method = method;
+        this->amount = amount;
+    }
+};
+
+class Colchon
+{
+private:
+    Nequi account;
+    int amount;
+    bool locked;
+
+public:
+    Colchon() {}
+    Colchon(Nequi account, int amount)
+    {
+        this->account = account;
+        this->amount = amount;
+        this->locked = false;
+    }
+};
+
+class Meta
+{
+private:
+    Nequi account;
+    int days;
+    int amount;
+
+public:
+    Meta() {}
+    Meta(Nequi account, int amount, int days)
+    {
+        this->account = account;
+        this->amount = amount;
+        this->days = days;
+    }
+};
+
+class Bolsillos
+{
+private:
+    Nequi account;
+    int amount;
+
+public:
+    Bolsillos() {}
+    Bolsillos(Nequi account, int amount)
+    {
+        this->account = account;
+        this->amount = amount;
+    }
+};
+
+class Retirar
+{
+private:
+    Nequi account;
+    int amount;
+
+public:
+    Retirar() {}
+    Retirar(Nequi account, int amount)
+    {
+        this->account = account;
+        this->amount = amount;
+    }
+};
+
+class Movimientos
+{
+private:
+    Nequi account;
+
+public:
+    Movimientos() {}
+    Movimientos(Nequi account)
+    {
+        this->account = account;
     }
 };
 
 int main()
 {
-    Nequi nequi = Nequi("3125198270", "0505");
     return 0;
 }
