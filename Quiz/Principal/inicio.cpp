baafca995688fd4f545e23fcc3e5ee76443a96fc
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-03-06 18:41:07 -0500

    Se creó el constructor de la clase Inicio y se agregó el método run en donde se ejecutara lo pedido en los retos 1, 2 y 3

--- Quiz/Principal/inicio.cpp
+++ Quiz/Principal/inicio.cpp
@@ -4,31 +4,44 @@
 
 using namespace std;
 
+class Inicio
+{
+private:
+    Persona p;
+
+public:
+    Inicio(){};
+    void run()
+    {
+        this->p.pedirDatos();
+        this->p.mostrarPersona();
+        if (this->p.mayorEdad())
+        {
+            cout << "Es mayor de edad" << endl;
+        }
+        else
+        {
+            cout << "Es menor de edad" << endl;
+        }
+        string peso = this->p.calcularIMC();
+        if (peso == "PESOBAJO")
+        {
+            cout << "El peso esta por debajo de lo ideal" << endl;
+        }
+        else if (peso == "PESOIDEAL")
+        {
+            cout << "El peso es ideal" << endl;
+        }
+        else
+        {
+            cout << "El peso esta por encima de lo ideal" << endl;
+        }
+    }
+};
+
 int main()
 {
-    Persona persona1;
-    persona1.pedirDatos();
-    persona1.mostrarPersona();
-    if (persona1.mayorEdad())
-    {
-        cout << "Es mayor de edad" << endl;
-    }
-    else
-    {
-        cout << "Es menor de edad" << endl;
-    }
-    string peso = persona1.calcularIMC();
-    if (peso == "PESOBAJO")
-    {
-        cout << "El peso esta por debajo de lo ideal" << endl;
-    }
-    else if (peso == "PESOIDEAL")
-    {
-        cout << "El peso es ideal" << endl;
-    }
-    else
-    {
-        cout << "El peso esta por encima de lo ideal" << endl;
-    }
+    Inicio inicio;
+    inicio.run();
     return 0;
 }
