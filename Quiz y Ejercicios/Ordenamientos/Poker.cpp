a824a92e7c63afd85cf30cd09eebdb768724199d
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-03-22 09:29:41 -0500

    Ordenar lista de cartas por valor

--- Quiz y Ejercicios/Ordenamientos/Poker.cpp
+++ Quiz y Ejercicios/Ordenamientos/Poker.cpp
@@ -3,68 +3,16 @@
 
 using namespace std;
 
-/* class Poker
-{
-private:
-    string name;
-    string suit;
-    int value;
-    string deck[52];
-
-public:
-    Poker(){};
-    Poker(string name, string suit, int value, list<string> deck) : name(name), suit(suit), value(value), deck(deck){};
-    ~Poker() {
-        delete[] deck;
-    }
-    void initDeck(const char *poker[], list<string> &deck)
-    {
-        string val = "";
-        while (val != "-1") {
-            cout << "Ingrese el valor de la carta: ";
-            cin >> val;
-            if (val != "-1") {
-                deck.push_back(val);
-            }
-        }
-    }
-    bool isInDeck(const string card)
-    {
-        for (auto it = this->deck->begin(); it != this->deck->end(); it++)
-        {
-            if ((string*)*it == &card) return true;
-        }
-        return false;
-    }
-    void showCard(const string &card)
-    {
-        if (isInDeck(card))
-        {
-            cout << "La carta " << card << " está en el deck" << endl;
-        }
-        else
-        {
-            cout << "La carta " << card << " no está en el deck" << endl;
-        }
-    }
-
-    void bubbleSort()
-    {
-        deck.sort();
-    }
-
-}; */
-
 struct Node
 {
-    string val;
+    int val;
     string suit;
     Node *next;
-    Node() : val(""), suit(""), next(nullptr){};
-    Node(string val, string suit) : val(val), suit(suit), next(nullptr){};
+    Node() : val(0), suit(""), next(nullptr){};
+    Node(int val, string suit) : val(val), suit(suit), next(nullptr){};
 };
 
-void insertOnInit(Node *&head, string val, string suit)
+void insertOnInit(Node *&head, int val, string suit)
 {
     Node *newNode = new Node(val, suit);
     newNode->next = head;
@@ -80,7 +28,7 @@ void printList(Node *head)
     }
 }
 
-void bubbleSort(Node* &head) {
+void bubbleSortSuits(Node* &head) {
     if (head == nullptr || head->next == nullptr) return;
     Node *current = head;
     Node *sorted = nullptr;
@@ -103,24 +51,50 @@ void bubbleSort(Node* &head) {
     head = sorted;
 }
 
+void bubbleSortValues(Node* &head) {
+    if (head == nullptr || head->next == nullptr) return;
+    Node *current = head;
+    Node *sorted = nullptr;
+
+    while (current != nullptr){
+        Node *next = current->next;
+        if (sorted == nullptr || sorted->val >= current->val) {
+            current->next = sorted;
+            sorted = current;
+        } else {
+            Node *temp = sorted;
+            while (temp->next != nullptr && temp->next->val < current->val) {
+                temp = temp->next;
+            }
+            current->next = temp->next;
+            temp->next = current;
+        }
+        current = next;
+    }
+    head = sorted;
+}
+
 int main()
 {
     Node *head = nullptr;
-    insertOnInit(head, "5", "Diamantes");
-    insertOnInit(head, "2", "Corazones");
-    insertOnInit(head, "4", "Picas");
-    insertOnInit(head, "3", "Treboles");
-    insertOnInit(head, "1", "Corazones");
-    insertOnInit(head, "6", "Diamantes");
-    insertOnInit(head, "7", "Picas");
-    insertOnInit(head, "8", "Treboles");
-    insertOnInit(head, "9", "Corazones");
+    insertOnInit(head, 5, "Diamantes");
+    insertOnInit(head, 2, "Corazones");
+    insertOnInit(head, 4, "Picas");
+    insertOnInit(head, 3, "Treboles");
+    insertOnInit(head, 1, "Corazones");
+    insertOnInit(head, 6, "Diamantes");
+    insertOnInit(head, 7, "Picas");
+    insertOnInit(head, 8, "Treboles");
+    insertOnInit(head, 9, "Corazones");
 
 
     cout << "Original: " << endl;
     printList(head);
-    cout << "Ordenada: " << endl;
-    bubbleSort(head);
+    cout << "Ordenada por traje: " << endl;
+    bubbleSortSuits(head);
+    printList(head);
+    cout << "Ordenada por valor: " << endl;
+    bubbleSortValues(head);
     printList(head);
     return 0;
 }
